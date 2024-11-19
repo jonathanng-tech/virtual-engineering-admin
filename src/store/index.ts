1f20c8b8d8100b4d1cb1b40324f01ddeaad91154
Jonathan <jonathan@ncubelabs.com>
2024-11-19 17:46:02 +0700

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,1 @@
-export { drawerStore } from './storeDrawer';
-export { productStore } from './storeProducts';
 export { useBrandStore } from './useBrandStore';
-export { useStyleStore } from './useStyleStore';
-export { useCategoriesStore } from './useCategoriesStore';
